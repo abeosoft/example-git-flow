ee96c2a2d397b79070d2b6fe3051290963748358
Adam <adam.janicki@roche.com>
2013-10-04 10:48:16 +0100

    Reset author

---
+++ src/main/java/com/example/gitflow/Dummy.java
@@ -0,0 +1,10 @@
+package com.example.gitflow;
+
+/**
+ * User: janickia
+ */
+public class Dummy {
+    public static void main(String[] args) {
+        System.out.println("It works!");
+    }
+}
---
+++ src/test/java/com/example/gitflow/DummyTest.java
@@ -0,0 +1,25 @@
+package com.example.gitflow;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * User: janickia
+ */
+public class DummyTest {
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testMain() throws Exception {
+
+    }
+}
